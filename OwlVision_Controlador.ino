a4ee70103ec29d7b429421c441a69893312f8d1b
MatheusSantos2 <50217083+MatheusSantos2@users.noreply.github.com>
2023-11-25 18:08:46 -0300

    Inserindo imagens e código atualizado

--- OwlVision_Controlador.ino
+++ OwlVision_Controlador.ino
@@ -1,156 +1,52 @@
+//----------------------------------------------------------------LIBRARIES-------------------------------------------------
+
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <WiFiServer.h>
 #include <RoboCore_Vespa.h>
-#include <PID_v1.h>
-
 #include <vector>
 #include <array>
 
+//Servidor TCP/IP
 const char* ssid = "Apto 303";
 const char* password = "P@$303W0rd";
 int serverPort = 80;
 
 WiFiServer server(serverPort);
 WiFiClient client;
+
 VespaMotors motorController;
-
-double targetX = 0; // Coordenada x desejada
-double targetY = 0; // Coordenada y desejada
-double currentX = 0; // Coordenada x atual
-double currentY = 0; // Coordenada y atual
-double targetSpeed = 0; // Velocidade desejada
-double currentSpeed = 0; // Velocidade atual
-int nextPointIndex = 0;
-int numPoints = 0;
-
-unsigned long previousTime = 0;
-unsigned long currentTime = 0;
-unsigned long lastTrajectoryTime = 0;
-unsigned long trajectoryTimeout = 5000; // Tempo limite para receber uma nova lista de posições (em milissegundos)
-
-double previousX = 0;
-double previousY = 0;
-
 std::vector<std::array<double, 2>> trajectoryPoints;
 
-class PIDController {
-private:
-  double* input;
-  double* output;
-  double* setpoint;
-  double Kp;
-  double Ki;
-  double Kd;
-  int direction;
+TaskHandle_t taskTrajectory;
 
-  PID pid;
+int nextPointIndex = 0;
+int numPoints = 0;
+double currentX = 0; 
+double currentY = 0;
 
-public:
-  PIDController(double* input, double* setpoint, double* output, double Kp, double Ki, double Kd, int direction)
-    : input(input), output(output), setpoint(setpoint), Kp(Kp), Ki(Ki), Kd(Kd), direction(direction), pid(PID(input, output, setpoint, Kp, Ki, Kd, direction))
-  {
-  }
+double targetX = 0; 
+double targetY = 0; 
+double defaultSpeed = 5;
+double distanceBetweenWheels = 13;
+double tolerance = 1;
 
-  void compute()
-  {
-    pid.Compute();
-  }
+String lastTrajectoryData = "data"; 
+String trajectoryData = "";
 
-  void setTunings(double Kp, double Ki, double Kd)
-  {
-    pid.SetTunings(Kp, Ki, Kd);
-  }
+//-------------------Desligar Motores-------------------------------//
+const unsigned long timeout = 90000;  // Tempo limite de 9 segundos
+unsigned long lastMessageTime = 0;
+bool systemEnabled = true;
 
-  void setOutputLimits(double min, double max)
-  {
-    pid.SetOutputLimits(min, max);
-  }
+//----------------------------------------------------------------SETUP-------------------------------------------------
 
-  void setMode(int mode)
-  {
-    pid.SetMode(mode);
-  }
-};
-
-// Controladores PI
-PIDController posXController(&currentX, &targetX, &currentSpeed, 2, 5, 0, DIRECT);
-PIDController posYController(&currentY, &targetY, &currentSpeed, 2, 5, 0, DIRECT);
-PIDController speedController(&currentSpeed, &targetSpeed, &currentSpeed, 2, 5, 0, DIRECT);
-
-
-void setup() 
-{
+void setup() {
   inicializeServer();
-  inicializePIDController();
+  xTaskCreatePinnedToCore(taskTrajectoryFunction, "TaskTrajectory", 10000, NULL, 1, &taskTrajectory, 1);
 }
 
-void loop() 
-{
-  connectServer();
-
-  // Verifica se há uma nova lista de posições recebidas
-  if (millis() - lastTrajectoryTime > trajectoryTimeout) {
-    // Se o tempo limite for excedido, pare os motores
-    motorController.stop();
-  }
-  
-  updateCurrentPosition();
-
-   if (reachedNextPoint()) {
-    // Atualiza o próximo ponto da trajetória
-    updateNextPoint();
-  }
-
-  processCurrentSpeed();
-  // Executa os controladores PI de posição
-  posXController.compute();
-  posYController.compute();
-
-  // Calcula a velocidade desejada a partir das coordenadas x e y desejadas
-  targetSpeed = sqrt(pow(targetX - currentX, 2) + pow(targetY - currentY, 2));
-
-  // Executa o controlador PI de velocidade
-  speedController.compute();
-
-  // Mapeia os valores de velocidade e direção para o intervalo de -100 a 100
-  int mappedSpeedLeft = map(currentSpeed, -100, 100, -255, 255);
-  int mappedSpeedRight = map(currentSpeed, -100, 100, -255, 255);
-
-  // Define a diferença de velocidade desejada entre os motores (pode ser ajustada)
-  int mappedSpeedLeftAdjusted = mappedSpeedLeft;
-  int mappedSpeedRightAdjusted = mappedSpeedRight;
-
-  if (targetX != currentX || targetY != currentY)
-   {
-    double deltaX = targetX - currentX;
-    double deltaY = targetY - currentY;
-    double angle = atan2(deltaY, deltaX);
-
-    // Calcula a diferença de velocidade entre os motores com base no ângulo
-    double speedDifferenceFactor = abs(cos(angle));
-    mappedSpeedLeftAdjusted = speedDifferenceFactor * mappedSpeedLeft;
-    mappedSpeedRightAdjusted = mappedSpeedRight;
-    
-    if (deltaY < 0) {
-      // Inverte a diferença de velocidade se o movimento for para trás
-      mappedSpeedLeftAdjusted = -mappedSpeedLeftAdjusted;
-    }
-  }
-
-  // Atualiza os motores com as velocidades ajustadas
-  motorController.setSpeedLeft(mappedSpeedLeftAdjusted);
-  motorController.setSpeedRight(mappedSpeedRightAdjusted);
-
-  // Aguarda um intervalo de tempo antes de repetir o loop
-
-  sendMessage("Velocidade Alvo:" + String(targetSpeed) + ", PosicaoAlvoX:" + String(targetX) + ", PosicaoAlvoY:" + String(targetY));
-
-  delay(100);
-}
-
-void inicializeServer()
-{
+void inicializeServer(){
   Serial.begin(115200);
   WiFi.begin(ssid, password);
 
@@ -169,79 +65,76 @@ void inicializeServer()
   Serial.println(serverPort);
 }
 
-void inicializePIDController()
-{
-  posXController.setMode(AUTOMATIC);
-  posYController.setMode(AUTOMATIC);
-  speedController.setMode(AUTOMATIC);
-}
+//---------------------------------------------------------------TASKS--------------------------------------------------
 
-void connectServer()
-{
-  // Verifica se há novas conexões de clientes
-  if (server.hasClient()) 
-  {
-    // Se há um cliente novo, desconecta o cliente anterior
-    if (client.connected()) 
-    {
-      client.stop();
+void taskTrajectoryFunction(void* parameter){
+  while (true) {
+    if (client.available()) {
+      String data = client.readStringUntil('\n');
+
+      String trajectoryLabel = data.substring(0, data.indexOf(';'));
+      trajectoryLabel.trim();
+      trajectoryData = removeFirstPosition(data);
+      trajectoryData.trim();
+
+      if (trajectoryLabel == "Trajectory" && lastTrajectoryData != trajectoryData && numPoints == 0) 
+      {
+        Serial.println("Sucess - taskTrajectoryFunction - Label: " + trajectoryLabel + "; Data: " + trajectoryData);
+
+        processTrajectoryData(trajectoryData);
+        lastTrajectoryData = trajectoryData;
+        resetTimer();
+      }
     }
-    // Aceita a nova conexão
-    client = server.available();
-    Serial.println("Nova conexao efetuada!");
-    sendMessage("Nova conexao efetuada!");
-  }
-
-  // Verifica se há dados disponíveis para leitura no cliente TCP
-  if (client.available()) 
-  {
-    // Lê os dados recebidos
-    String data = client.readStringUntil('\n');
-    // Processa os dados recebidos
-    processTrajectoryData(data);
-    lastTrajectoryTime = millis();
+    vTaskDelay(pdMS_TO_TICKS(100)); // Aguardar 10 milissegundos antes de verificar novamente
   }
 }
 
-void processTrajectoryData(String data) {
-  // Limpa o vetor de trajetória antes de receber os novos pontos
+String removeFirstPosition(String data){
+  int index = 0;
+  index = data.indexOf(';');
+  if (index != -1){
+    data = data.substring(index + 1);  // Extrai apenas os valores das coordenadas
+  }
+  return data;
+}
+
+void processTrajectoryData(String data){
+
+  Serial.println("processTrajectoryData - Inicia o processamento da lista de posicoes");
+
   trajectoryPoints.clear();
 
-  // Extrai os pontos de trajetória da string recebida
   String point;
   int index = 0;
 
-  while ((index = data.indexOf(';')) != -1) 
-  {
+  while((index = data.indexOf(';')) != -1){
     point = data.substring(0, index);
     data = data.substring(index + 1);
 
     String xCoordinate = point.substring(0, point.indexOf(','));
     String yCoordinate = point.substring(point.indexOf(',') + 1);
 
-    std::array<double, 2> newPoint;
-    newPoint[0] = xCoordinate.toDouble();
-    newPoint[1] = yCoordinate.toDouble();
+    std::array<double, 2> newPoint = {xCoordinate.toDouble(), yCoordinate.toDouble()};
 
-    // Adiciona o novo ponto ao vetor de trajetória
     trajectoryPoints.push_back(newPoint);
   }
 
-  // Processa o último ponto após o último ';'
   String xCoordinate = data.substring(0, data.indexOf(','));
   String yCoordinate = data.substring(data.indexOf(',') + 1);
 
-  std::array<double, 2> newPoint;
-  newPoint[0] = xCoordinate.toDouble();
-  newPoint[1] = yCoordinate.toDouble();
+  std::array<double, 2> newPoint = {xCoordinate.toDouble(), yCoordinate.toDouble()};
 
-  // Adiciona o último ponto ao vetor de trajetória
   trajectoryPoints.push_back(newPoint);
 
-  int numPoints = trajectoryPoints.size();
-  // Imprime os pontos de trajetória recebidos
-  Serial.println("Pontos de Trajetória Recebidos:");
-  for (int i = 0; i < numPoints; i++) {
+  printReceveidPoints();
+}
+
+void printReceveidPoints(){
+  numPoints = 5;
+  Serial.println("printReceveidPoints - Pontos de Trajetória Recebidos: ");
+  
+  for (int i = 0; i < numPoints; i++){
     Serial.print("Ponto ");
     Serial.print(i);
     Serial.print(": X=");
@@ -251,69 +144,126 @@ void processTrajectoryData(String data) {
   }
 }
 
-// Função para atualizar a posição atual
-void updateCurrentPosition() {
-  // Verifica se a lista de pontos da trajetória está vazia
-  if (numPoints == 0) {
-    // Se estiver vazia, defina as coordenadas atuais como 0
-    currentX = 0;
-    currentY = 0;
-    return;
+//----------------------------------------------------------------LOOP-------------------------------------------------
+
+void loop(){
+  connectTrajectoryServer();
+  if(hasTimerExpired())
+    enginePause();
+
+  if (numPoints > 0 && systemEnabled){
+    engineControl();
   }
 
-  // Verifica se o índice do próximo ponto está fora dos limites da lista de pontos
-  if (nextPointIndex >= numPoints) {
-    // Se estiver fora dos limites, define as coordenadas atuais como as coordenadas do último ponto
-    currentX = trajectoryPoints[numPoints - 1][0];
-    currentY = trajectoryPoints[numPoints - 1][1];
-    return;
+  delay(100);
+}
+
+void connectTrajectoryServer(){
+  if (server.hasClient())
+  {
+    if (!client.connected())
+    {
+      client = server.available();
+      Serial.println("Nova conexão estabelecida com o cliente.");
+    }else{
+      server.stop();
+      server.begin();
+    }
+  }
+}
+
+void engineControl(){
+  double setpointX = readTargetPositionX();
+  double setpointY = readTargetPositionY();
+  
+  // Calcular a diferença entre as coordenadas do ponto alvo e do robô
+  double delta_x = setpointX - currentX;
+  double delta_y = setpointY - currentY;
+
+  // Calcular a orientação do ponto alvo em relação ao robô
+  double theta_target = atan2(delta_y, delta_x);
+
+  // Calcular as velocidades das rodas diretamente
+  double leftSpeed = defaultSpeed + (distanceBetweenWheels / 2) * sin(theta_target);
+  double rightSpeed = defaultSpeed - (distanceBetweenWheels / 2) * sin(theta_target);
+
+  Serial.println("loop - Velocidade Direita: " + String(rightSpeed) + "; Velocidade Esquerda: " + String(leftSpeed) + " Angle: " + theta_target);
+  
+  if((leftSpeed > 100 || leftSpeed < 0) && (rightSpeed > 100 || rightSpeed < 0))
+  {
+      accelerateMotors(rightSpeed, leftSpeed);
   }
 
-  // Obtém as coordenadas atuais da lista de pontos
-  currentX = trajectoryPoints[nextPointIndex][0];
-  currentY = trajectoryPoints[nextPointIndex][1];
+  delay(10000);
+  Serial.println("loop - Pausa motores");
+  motorController.stop();
+  
+  String message = String(leftSpeed) + "," + String(rightSpeed) + "," + String(setpointX) + "," + String(setpointY) + "," + String(currentX) + "," + String(currentY);
+  sendMessage(message);
+
+  currentX = setpointX; 
+  currentY = setpointY;
+
+  Serial.println("loop - spx: " + String(setpointX) + " ; spy: " + String(setpointY));
+  updateNextPoint();
 }
 
-// Função para verificar se alcançou o próximo ponto da trajetória
-bool reachedNextPoint() {
-  // Verifica se a distância até o próximo ponto é menor que uma tolerância
-  double distance = sqrt(pow(targetX - currentX, 2) + pow(targetY - currentY, 2));
-  return distance < 0.1; // Defina a tolerância adequada
-}
-
-// Função para atualizar o próximo ponto da trajetória
-void updateNextPoint() {
-  // Verifica se a lista de pontos da trajetória está vazia
-  if (numPoints == 0) {
-    // Se estiver vazia, defina os valores de targetX e targetY como 0
-    targetX = 0;
-    targetY = 0;
-    return;
+void updateNextPoint(){
+  if(numPoints > 1)
+  {
+    nextPointIndex = (nextPointIndex + 1) % numPoints;
+    
+    if(nextPointIndex > 4){
+      numPoints  = 0;
+    }
   }
-
-  // Incrementa o índice do próximo ponto
-  nextPointIndex = (nextPointIndex + 1) % numPoints;
-
-  // Obtém as coordenadas x e y do próximo ponto da trajetória
-  targetX = trajectoryPoints[nextPointIndex][0];
-  targetY = trajectoryPoints[nextPointIndex][1];
 }
 
-void processCurrentSpeed()
-{
-  // Calcula a velocidade com base na variação das coordenadas
-  currentTime = millis(); // Obtém o tempo atual em milissegundos
-  previousTime = millis();
-  double deltaTime = (currentTime - previousTime) / 1000.0; // Diferença de tempo em segundos
-  currentSpeed = sqrt(pow((currentX - previousX) / deltaTime, 2) + pow((currentY - previousY) / deltaTime, 2));
-
-  // Atualiza os valores anteriores para a próxima iteração
-  previousX = currentX;
-  previousY = currentY;
-  previousTime = currentTime;
+double readTargetPositionX(){
+      if (!trajectoryPoints.empty()) {
+        const auto& desiredPoint = trajectoryPoints[nextPointIndex];
+        return desiredPoint[0];
+    }
+    return 0;
 }
 
-void sendMessage(String message)
-{
+double readTargetPositionY(){
+    if (!trajectoryPoints.empty()) {
+        const auto& desiredPoint = trajectoryPoints[nextPointIndex];
+        return desiredPoint[1];
+    }
+    return 0;
+}
+
+void accelerateMotors(int rightEngine, int leftEngine){
+  //Serial.println("Acelerando motores - Motor Direito: " + String(rightEngine) + " Motor Esquerdo: " + String(leftEngine));
+  motorController.turn(rightEngine, leftEngine);
+}
+
+void sendMessage(String message){
+  Serial.println("sendMessage - " + message);
   client.println(message);
+}
+
+bool hasTimerExpired()
+{
+    return (millis() - lastMessageTime > timeout) && systemEnabled;
+}
+
+void resetTimer() {
+  lastMessageTime = millis();  
+  if (!systemEnabled) {
+    systemEnabled = true;
+  }
+}
+
+void enginePause(){
+  Serial.println("enginePause - Pausa Motores após muito tempo sem receber uma nova mensagem");
+  motorController.stop();
+  trajectoryPoints.clear();
+  nextPointIndex = 0;
+  numPoints = 0;
+  systemEnabled = false;
+
+  delay(1000);
 }
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# AutonomousVehicleController
+# OwlVision_VehicleController
+
+## 1. Introdução
+O OwlVision é o projeto de conclusão de curso que foi divido em três repositorios: 
+
+    * OwlVision_AppAndroid : Aplicativo android com função de captura de imagens, interpretação das imagens e geração de trajetórias, desenvolvido majoritariamente em Java.
+    * OwlVision_MLVisions: Conjunto de pipelines de processamento de imagens, como por exemplo, pipeline de geração dos modelos de estimação de profundidade e segmentação semantica, também a geração dos parametros intrinsecos da camera e outras pipelines auxiliares.
+    * OwlVision_VehicleController: Serviço que recebe os dados de trajetória e faz o controle do veiculo, atráves dos atuardores (motores).
+
+## 2. Estrutura Mecânica
+
+![Alt text](image.png)
+
+## 3. Estrutura Eletrônica
+
+![Alt text](image-1.png)
+
+## 4. Referências de Projeto
+https://www.robocore.net/tutoriais/controle-web-robo-com-vespa
